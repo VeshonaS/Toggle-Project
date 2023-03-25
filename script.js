b65431a8d08d92c9697849b63c001e8a67685677
VeshonaS <128939653+VeshonaS@users.noreply.github.com>
2023-03-25 19:02:55 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,47 @@
+$("#bulls").click(function() {
+$("#bull2").toggle();
+$("#red").toggle();
+});
+
+$("#laker").click(function() {
+$("#lal").fadeToggle();
+$("#boo").fadeToggle();
+});
+
+$("#golden").click(function() {
+$("#gsw").fadeToggle();
+$("#ya").fadeToggle();
+});
+
+$("#knick").click(function() {
+$("#nyk").slideToggle();
+$("#meh").slideToggle();
+});
+
+$("#nets").click(function() {
+$("#bkn").slideToggle();
+$("#rep").slideToggle();
+});
+
+$("#okc").click(function() {
+$("#oh-kc").fadeToggle();
+$("#yaay").fadeToggle();
+});
+
+$("#beanT").click(function() {
+$("#mbc").fadeToggle();
+$("#win").fadeToggle();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
